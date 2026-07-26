f0263920cf9058b83c91c5e01904b988cb71cdfc
So Mun <induwaramadushan20@gmail.com>
2026-07-26 10:50:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,5 +7,5 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.PORT === undefined ? "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=f274f071242996f8" : process.env.SESSION_NAME,
 };
